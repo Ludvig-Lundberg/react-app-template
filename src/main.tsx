185a915e35688c177349e6643db3e84367434445
Ludvig-Lundberg <ickeskola@gmail.com>
2023-06-13 18:18:20 +0200

    cleaned up code in 'main.tsx' & 'App.tsx', added basic route to app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<header></header>
+		<Routes>
+			<main>
+				<Route path="/">
+					<App />
+				</Route>
+			</main>
+		</Routes>
+		<footer></footer>
 	</React.StrictMode>
 );
